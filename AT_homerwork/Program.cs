ccad987a38c051f74695fca23e02befd369b2874
proninman <proninman@gmail.com>
2021-04-27 20:28:39 +0300

    Тестовый файл ВС

---
+++ AT_homerwork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AT_homerwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
