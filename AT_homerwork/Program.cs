c78ef3cc468970d3de3508cdf93e1fbf2a536e86
proninman <proninman@gmail.com>
2021-04-27 20:34:31 +0300

    Добавление заданий калькулятор и массив

--- AT_homerwork/Program.cs
+++ AT_homerwork/Program.cs
@@ -6,7 +6,75 @@ namespace AT_homerwork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char again = 'y';
+            while (again == 'y')
+            {
+                double a;
+                double b;
+                double total;
+                char oper;
+                double x;
+
+                Console.WriteLine("Введите первое число:");
+                if (Double.TryParse(Console.ReadLine(), out x))
+                {
+                    a = x;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод первого оператора");
+                    break;
+                }
+
+                Console.WriteLine("Введите оператор:");
+                oper = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("Введите второе число:");
+
+                if (Double.TryParse(Console.ReadLine(), out x))
+                {
+                    b = x;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод второго оператора");
+                    break;
+                }
+
+
+                switch (oper)
+                {
+
+                    case '+':
+                        total = a + b;
+                        Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+                        break;
+
+
+                    case '-':
+                        total = a - b;
+                        Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
+                        break;
+
+                    case '*':
+                        total = a * b;
+                        Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
+                        break;
+
+                    case '/':
+                        total = a / b;
+                        Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+                        break;
+
+
+                    default:
+                        Console.WriteLine("Неизвестный оператор.");
+                        break;
+                };
+
+                Console.WriteLine("Вы хотите продолжить работу с калькулятором? (y/n)");
+                again = Convert.ToChar(Console.ReadLine());
+            }
+
         }
     }
 }
---
+++ mass/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace mass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размерность массива: ");
+            int N = int.Parse(Console.ReadLine());
+
+            int[] A = new int[N];
+
+            Random rand = new Random();
+
+            Console.WriteLine("Массив, заполненный случайным образом:");
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = rand.Next(0, 100);
+                Console.Write(" " + A[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine(new String('-', 50));
+
+            int temp;
+            for (int i = 0; i < A.Length - 1; i++)
+            {
+                for (int j = i + 1; j < A.Length; j++)
+                {
+                    if (A[i] > A[j])
+                    {
+                        temp = A[i];
+                        A[i] = A[j];
+                        A[j] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Вывод отсортированного по возрастанию массива");
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write(" " + A[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine(new String('-', 50));
+            Console.ReadLine();
+        }
+    }
+}
